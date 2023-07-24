e5c5e003db5b47154552307e176e89561586ad62
NaveenprasanthT <naveenprasanth2109@gmail.com>
2023-07-24 16:27:14 +0530

    Spacewood

--- src/pages/Spacewood.js
+++ src/pages/Spacewood.js
@@ -3,6 +3,8 @@ import Review from '@/Components/Home/Review';
 import FollowUs from '@/Components/Home/followUs';
 import Franchises from '@/Components/Home/franchises';
 import Navbar from '@/Components/Home/navbar';
+import SpacewoodMain from '@/Components/Spacewood/SpacewoodMain';
+import TextImage from '@/Components/Spacewood/TextImage';
 import TasselsFooter from '@/Components/footer';
 import React from 'react';
 
@@ -10,6 +12,8 @@ function Spacewood() {
     return ( 
         <>
             <Navbar />
+            <TextImage />
+            <SpacewoodMain />
             <Franchises />
             <FollowUs />
             <Review />
